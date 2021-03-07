c92ea8c532b7e6cadc47ebe545f389ad812b5db4
mmukul0409 <mukul.mangla@gmail.com>
2021-03-07 12:44:44 +0530

    Task 2: Adding instant search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -10,6 +10,7 @@ import {
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
 import { Observable } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
@@ -34,6 +35,13 @@ export class BookSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.books$ = this.store.select(getAllBooks);
+
+    this.searchForm
+      .get('term')
+      .valueChanges.pipe(debounceTime(500))
+      .subscribe(value => {
+        this.searchBooks();
+      });
   }
 
   addBookToReadingList(book: Book) {
